82eb3350d872dde3d57b68ee36d2ddd2ee42b5af
Muchmorepig <muchmorepig@gmail.com>
2024-10-10 16:01:00 +0800

    all

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Mu-chun's Personal Bioinformatics Tools")
+}
+
+#.onLoad <- function(libname, pkgname) {
+#  packageStartupMessage("Mu-chun's Personal Bioinformatics Tools")
+#  # message("Mu-chun's Personal Bioinformatics Tools")
+#}
